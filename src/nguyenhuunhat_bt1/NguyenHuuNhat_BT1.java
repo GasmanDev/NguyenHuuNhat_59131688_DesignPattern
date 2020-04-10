e9a8e0ed2948f68b07098860e63645d6e3eb7fa6
Nguyen Huu Nhat <huunhat07@gmail.com>
2020-04-10 20:43:36 +0700

    Create QLDS class, testcase

--- src/nguyenhuunhat_bt1/NguyenHuuNhat_BT1.java
+++ src/nguyenhuunhat_bt1/NguyenHuuNhat_BT1.java
@@ -15,7 +15,20 @@ public class NguyenHuuNhat_BT1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic 
+        QLDS ds = new QLDS();
+        HocSinh test = new HocSinh("Nguyễn Hữu Nhật", 21, "Nguyễn Đình Chiểu", "0912181370", "59-CNTT1", "Game");
+        //test.hienThiTT();
+        if (ds.them(test) == 1)
+        {
+            System.out.println("Them thanh cong");
+            ds.inDS();
+        }
+        if (ds.xoa("Nguyễn Hữu Nhật") == 1)
+        {
+           System.out.println("Xoa thanh cong");
+           ds.inDS();
+        }   
     }
     
 }
