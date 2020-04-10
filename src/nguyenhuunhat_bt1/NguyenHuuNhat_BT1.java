a623a5923b85a19bd9014d9ff0fb84c0d237deed
Nguyen Huu Nhat <huunhat07@gmail.com>
2020-04-10 20:50:47 +0700

    Add class lophoc

--- src/nguyenhuunhat_bt1/NguyenHuuNhat_BT1.java
+++ src/nguyenhuunhat_bt1/NguyenHuuNhat_BT1.java
@@ -16,19 +16,25 @@ public class NguyenHuuNhat_BT1 {
      */
     public static void main(String[] args) {
         // TODO code application logic 
-        QLDS ds = new QLDS();
-        HocSinh test = new HocSinh("Nguyễn Hữu Nhật", 21, "Nguyễn Đình Chiểu", "0912181370", "59-CNTT1", "Game");
-        //test.hienThiTT();
-        if (ds.them(test) == 1)
-        {
-            System.out.println("Them thanh cong");
-            ds.inDS();
-        }
-        if (ds.xoa("Nguyễn Hữu Nhật") == 1)
-        {
-           System.out.println("Xoa thanh cong");
-           ds.inDS();
-        }   
+//        QLDS ds = new QLDS();
+        LopHoc LH = new LopHoc();
+        HocSinh hs1 = new HocSinh("Nguyễn Hữu Nhật", 21, "Nguyễn Đình Chiểu", "0912181370", "59-CNTT1", "Game");
+        GiaoVien gv1 = new GiaoVien("Giáo Viên", 30, "Phạm Văn Đồng", "0123456789", "DesignPattern", "Toán-Tin");
+        LH.themHocSinh(hs1);
+        LH.themGVGD(gv1);
+        LH.inDSHS();
+        LH.inDSGVDG();
+//test.hienThiTT();
+//        if (ds.them(test) == 1)
+//        {
+//            System.out.println("Them thanh cong");
+//            ds.inDS();
+//        }
+//        if (ds.xoa("Nguyễn Hữu Nhật") == 1)
+//        {
+//           System.out.println("Xoa thanh cong");
+//           ds.inDS();
+//        }   
     }
     
 }
